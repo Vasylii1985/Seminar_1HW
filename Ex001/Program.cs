31a880e11fcfcded8a49675d0da2e6ac6262e344
Vasilii <bespalchuk1985@gmail.com>
2022-04-17 21:28:21 +0300

    Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+int min = n1;
+
+if (n1 > max) max = n1;
+if (n1 < max) min = n1;
+if (n2 > max) max = n2;
+if (n2 < max) min = n2;
+
+Console.WriteLine("max = " +max);
+Console.WriteLine("min = " +min);
